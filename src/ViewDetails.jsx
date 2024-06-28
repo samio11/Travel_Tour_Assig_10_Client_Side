f68928edfc1695804bdc0c77ab8d8e2ae6aa301f
Samio Hasan <138763106+samio11@users.noreply.github.com>
2024-06-28 23:00:33 +0600

    View page added

--- src/ViewDetails.jsx
+++ src/ViewDetails.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { TiWorld } from "react-icons/ti";
+import { MdMonetizationOn } from "react-icons/md";
+import { FaPeopleGroup } from "react-icons/fa6";
 
 const ViewDetails = () => {
     const data = useLoaderData();
+    const navigate = useNavigate();
     console.log(data);
+    const handleBack = () => {
+        navigate(-1);
+    }
     return (
         <div>
             <div className="hero bg-base-200 h-auto lg:h-[80vh]">
@@ -13,11 +20,29 @@ const ViewDetails = () => {
                         className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
                         <h1 className="text-5xl font-bold">{data.touristSpotName}</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <div className='flex justify-start items-center gap-3 my-4'>
+                            <TiWorld size={24} className="mr-2" />
+                            <span>{data.countryName}</span>
+                        </div>
+                        <div className='flex justify-center items-center gap-3 my-4'>
+                            <span>{data.shortDescription}</span>
+                        </div>
+                        <div className='flex justify-start items-center gap-3 my-4'>
+                            <MdMonetizationOn size={24} className="mr-2" />
+                            <span>{data.averageCost}$ ~ {data.travelTime}</span>
+                        </div>
+                        <div className='flex justify-start items-center gap-3 my-4'>
+                            <FaPeopleGroup size={24} className="mr-2" />
+                            <span>{data.totalVisitorsPerYear}/year</span>
+                        </div>
+                        <div className='flex justify-start items-center gap-3 my-4'>
+                            Season to go:-
+                            <span>{data.seasonality}</span>
+                        </div>
+
+                        <div className='flex justify-center items-center'>
+                            <button onClick={handleBack} className="btn btn-outline">Back</button>
+                        </div>
                     </div>
                 </div>
             </div>
